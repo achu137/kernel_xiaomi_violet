f53d59a18349fc3615b8b42d45fae06083d23d7b
Alexander Winkowski <dereference23@outlook.com>
2023-03-19 06:17:00 +0000

    trace: Fix compilation without CONFIG_NO_HZ
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -398,6 +398,7 @@ TRACE_EVENT(sched_load_balance,
 		__entry->balance_interval, __entry->active_balance)
 );
 
+#ifdef CONFIG_NO_HZ_COMMON
 TRACE_EVENT(sched_load_balance_nohz_kick,
 
 	TP_PROTO(int cpu, int kick_cpu),
@@ -427,6 +428,7 @@ TRACE_EVENT(sched_load_balance_nohz_kick,
 			__entry->kick_cpu, __entry->nohz_flags)
 
 );
+#endif
 
 TRACE_EVENT(sched_load_balance_sg_stats,
 
