62b4cd2f7e4b8211afea10268bcd3117c9325ab1
shygosh <mossynine@gmail.com>
2024-02-22 12:23:24 +0700

    Revert "mm: retry more before OOM in the presence of slow shrinkers"
    This reverts commit 23ba63a1c1039880c9e6ef069f4261f9ccc575b2.
    
    Signed-off-by: shygosh <shygosh@proton.me>
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -478,7 +478,6 @@ static void exit_mm(void)
 {
 	struct mm_struct *mm = current->mm;
 	struct core_state *core_state;
-	int mm_released;
 
 	exit_mm_release(current, mm);
 	if (!mm)
@@ -529,15 +528,13 @@ static void exit_mm(void)
 	task_unlock(current);
 	mm_update_next_owner(mm);
 
-	mm_released = mmput(mm);
+	mmput(mm);
 #ifdef CONFIG_ANDROID_SIMPLE_LMK
 	clear_thread_flag(TIF_MEMDIE);
 #else
 	if (test_thread_flag(TIF_MEMDIE))
 		exit_oom_victim();
 #endif
-	if (mm_released)
-		set_tsk_thread_flag(current, TIF_MM_RELEASED);
 }
 
 static struct task_struct *find_alive_thread(struct task_struct *p)
